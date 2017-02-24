73d33f4801f1493f9db606e23234135aa6110461
sofiaknezevic <sofia.knezevic@gmail.com>
2017-02-24 14:04:07 -0800

    Initial commit

---
+++ AVStoryBook/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AVStoryBook
+//
+//  Created by Sofia Knezevic on 2017-02-24.
+//  Copyright © 2017 Sofia Knezevic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AVStoryBook/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AVStoryBook
+//
+//  Created by Sofia Knezevic on 2017-02-24.
+//  Copyright © 2017 Sofia Knezevic. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
